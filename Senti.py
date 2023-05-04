8a5580f1f320ae0733520e3c6245f2ee36d0aad6
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-05-05 03:55:54 +0530

    Update Senti.py

--- Senti.py
+++ Senti.py
@@ -1,6 +1,8 @@
 import csv
 import re
 import pandas as pd
+import nltk
+nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import plotly.express as px
 import plotly.graph_objects as go
