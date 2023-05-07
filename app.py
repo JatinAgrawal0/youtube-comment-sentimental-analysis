73e6b7d1d8ab63543110ff46e28aefabb16caff0
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-05-07 22:53:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
